627ee4503f1a03bd6387175cacde1f767956e48b
Richardson <richardson-santiago@hotmail.com>
2017-10-01 16:00:40 +0000

    Classificador svm

--- svm/svm.cpp
+++ svm/svm.cpp
@@ -1,68 +1,70 @@
-#include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/ml/ml.hpp>
+//OpenCV
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include "opencv2/imgcodecs.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/ml.hpp>
 
 using namespace cv;
+using namespace cv::ml;
 
-int main()
+int main(int, char **)
 {
-    // Data for visual representation
+    // Representacao visual dos dados 
     int width = 512, height = 512;
     Mat image = Mat::zeros(height, width, CV_8UC3);
 
-    // Set up training data
-    float labels[4] = {1.0, -1.0, -1.0, -1.0};
-    Mat labelsMat(4, 1, CV_32FC1, labels);
+    // dados de treinamento 
+    int labels[15] = {1, -1, -1, -1, 1, -1, 1, 1, 1, 1, 1, -1, -1, -1, -1};
+    float trainingData[15][2] = {{501, 10}, {255, 10}, {501, 255},
+                                 {10, 501}, {501, 40}, {200, 22}, 
+                                 {450, 30}, {392, 432}, {250, 459},
+                                 {490, 210}, {490, 490}, {30, 50},
+                                 {260, 120}, {8, 200}, {230, 140}                                
+                                };
 
-    float trainingData[4][2] = { {501, 10}, {255, 10}, {501, 255}, {10, 501} };
-    Mat trainingDataMat(4, 2, CV_32FC1, trainingData);
+    //Dados como objetos da classe Mat                            
+    Mat trainingDataMat(15, 2, CV_32FC1, trainingData);
+    Mat labelsMat(15, 1, CV_32SC1, labels);
 
-    // Set up SVM's parameters
-    CvSVMParams params;
-    params.svm_type    = CvSVM::C_SVC;
-    params.kernel_type = CvSVM::LINEAR;
-    params.term_crit   = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
+    // Treinamento da SVM
+    Ptr<SVM> svm = SVM::create();
+    svm->setType(SVM::C_SVC);
+    svm->setKernel(SVM::LINEAR);
+    svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
+    svm->train(trainingDataMat, ROW_SAMPLE, labelsMat);
 
-    // Train the SVM
-    CvSVM SVM;
-    SVM.train(trainingDataMat, labelsMat, Mat(), Mat(), params);
-
-    Vec3b green(0,255,0), blue (255,0,0);
-    // Show the decision regions given by the SVM
+    // Mostra as regioes separadas pela svm
+    Vec3b area1(170, 232, 238), area2(238, 238, 175);
     for (int i = 0; i < image.rows; ++i)
         for (int j = 0; j < image.cols; ++j)
         {
-            Mat sampleMat = (Mat_<float>(1,2) << j,i);
-            float response = SVM.predict(sampleMat);
-
+            Mat sampleMat = (Mat_<float>(1, 2) << j, i);
+            float response = svm->predict(sampleMat);
             if (response == 1)
-                image.at<Vec3b>(i,j)  = green;
+                image.at<Vec3b>(i, j) = area1;
             else if (response == -1)
-                 image.at<Vec3b>(i,j)  = blue;
+                image.at<Vec3b>(i, j) = area2;
         }
 
     // Show the training data
-    int thickness = -1;
+    int thickness = 1;
     int lineType = 8;
-    circle( image, Point(501,  10), 5, Scalar(  0,   0,   0), thickness, lineType);
-    circle( image, Point(255,  10), 5, Scalar(255, 255, 255), thickness, lineType);
-    circle( image, Point(501, 255), 5, Scalar(255, 255, 255), thickness, lineType);
-    circle( image, Point( 10, 501), 5, Scalar(255, 255, 255), thickness, lineType);
-
+    for(int i=0; i<15; i++){
+        circle(image, Point((int)trainingData[i][0], (int)trainingData[i][1]), 5, Scalar(132, 21, 69), thickness, lineType);
+    }
+  
     // Show support vectors
     thickness = 2;
-    lineType  = 8;
-    int c     = SVM.get_support_vector_count();
-
-    for (int i = 0; i < c; ++i)
+    lineType = 8;
+    Mat sv = svm->getUncompressedSupportVectors();
+    for (int i = 0; i < sv.rows; ++i)
     {
-        const float* v = SVM.get_support_vector(i);
-        circle( image,  Point( (int) v[0], (int) v[1]),   6,  Scalar(128, 128, 128), thickness, lineType);
+        const float *v = sv.ptr<float>(i);
+        circle(image, Point((int)v[0], (int)v[1]), 6, Scalar(0, 0, 255), thickness, lineType);
     }
 
     imwrite("result.png", image);        // save the image
-
     imshow("SVM Simple Example", image); // show it to the user
     waitKey(0);
-
 }
